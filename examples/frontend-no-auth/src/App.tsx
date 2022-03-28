8eb8faddf64bed03eb5348838250235887766bd5
Shohei Tanaka <shou.tanak@gmail.com>
2022-03-28 20:31:31 +0900

    remove comment

--- examples/frontend-no-auth/src/App.tsx
+++ examples/frontend-no-auth/src/App.tsx
@@ -3,7 +3,7 @@
 import { launch } from "@onboarden/importer";
 import React from "react";
 
-const TEMPLATE_ID = "YOUR TEMPLATE ID"; // change this
+const TEMPLATE_ID = "YOUR TEMPLATE ID";
 
 function App() {
   const launchImporter = () => {
