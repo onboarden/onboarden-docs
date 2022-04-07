5b10208035e8f183b21df0574589b91b072f849e
Shohei Tanaka <shou.tanak@gmail.com>
2022-04-07 18:28:31 +0900

    change angular setup script.

--- examples/angular/setup.sh
+++ examples/angular/setup.sh
@@ -1,2 +1,16 @@
 npx -p @angular/cli@latest ng new my-app
 npm install @onboarden/importer
+
+# change angular.json
+```
+{
+  ...
+  "serve": {
+    "options": {
+      "port": 3000 // add this config for CORS
+    },
+    "builder": "@angular-devkit/build-angular:dev-server",
+  ...
+},
+}
+```
