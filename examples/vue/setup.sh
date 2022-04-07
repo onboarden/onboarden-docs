176f58d03ea95d67293b287089d27b7fe4ef2bfa
takasay <takataka4072715@gmail.com>
2022-04-07 16:14:06 +0900

    Add vue example

---
+++ examples/vue/setup.sh
@@ -0,0 +1,12 @@
+npm init vue@latest
+
+✔ Project name: … vue
+✔ Add TypeScript? … Yes
+✔ Add JSX Support? … No
+✔ Add Vue Router for Single Page Application development? … No
+✔ Add Pinia for state management? … No
+✔ Add Vitest for Unit Testing? … No
+✔ Add Cypress for both Unit and End-to-End testing? … No
+✔ Add ESLint for code quality? … No
+
+npm install @onboarden/importer
---
+++ examples/vue/src/AppNoAuth.vue
@@ -0,0 +1,24 @@
+<!--  src/App.vue -->
+
+<script lang="ts">
+import { launch } from "@onboarden/importer";
+
+const TEMPLATE_ID = "YOUR_TEMPLATE_ID";
+
+export default {
+  methods: {
+    open: async () => {
+      launch({
+        templateId: TEMPLATE_ID,
+        onCompleted: (result) => console.log(result),
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <button @click="open">Import</button>
+  </main>
+</template>
