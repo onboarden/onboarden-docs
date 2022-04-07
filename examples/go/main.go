6a6070580bf524ec7e11ed5b9ae85df28b62c37e
Shohei Tanaka <shou.tanak@gmail.com>
2022-04-07 20:23:19 +0900

    add go sample

---
+++ examples/go/main.go
@@ -0,0 +1,71 @@
+// main.go
+
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"time"
+
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+)
+
+const API_KEY = "YOUR_API_KEY" // change this
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+type Response struct {
+	Token string `json:"token"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+		response := Response{Token: generateToken()}
+		
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		if err := enc.Encode(&response); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(buf.String())
+
+		_, err := fmt.Fprint(w, buf.String())
+		if err != nil {
+			return
+		}
+}
+
+func generateToken() string {
+	claims := jwt.MapClaims{
+		// あなたのアプリケーションのユーザーを一意に識別するID（任意）
+    // 付与すると、ユーザーがインポートを中断した場合でも再アクセス時に途中から再開できます
+		"sub": "user-id-of-your-app",
+		"exp": time.Now().Add(time.Hour * 1).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+
+	rawKey, err := base64.StdEncoding.DecodeString(API_KEY)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(rawKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenString;
+}
---
+++ examples/go/setup.sh
@@ -0,0 +1,2 @@
+go mod init hello
+go get github.com/golang-jwt/jwt@latest
