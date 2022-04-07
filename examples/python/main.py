b17e431e838807340917c983a7d90fc73b28ba00
Shohei Tanaka <shou.tanak@gmail.com>
2022-04-07 18:42:08 +0900

    fix angular and python sample. cors

--- examples/python/main.py
+++ examples/python/main.py
@@ -3,8 +3,14 @@ import time
 
 import jwt
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+)
 
 API_KEY = "YOUR API KEY"  # Change here!
 PRIVATE_KEY = base64.b64decode(API_KEY)
